0cfd1dab380363ae92db2b70d9648c3013c77ff2
Victor <victor.ciechovicz@merx.tech>
2025-10-20 18:48:06 -0300

    feat: create crud

---
+++ src/main/java/CadastroRepository.java
@@ -0,0 +1,92 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadastroRepository {
+
+    private Connection conexao;
+
+    public CadastroRepository() {
+        conexao = FabricaConexao.getConexao();
+    }
+
+    public void incluir(Cadastro cadastro) {
+        try {
+            String sql = "INSERT INTO public.tab_cadastro(nome, idade) VALUES(?,?)";
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            pst.setString(1, cadastro.getNome());
+            pst.setInt(2, cadastro.getIdade());
+            pst.execute();
+            System.out.println("Cadastro realizado com sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    ;
+
+    public void alterar(Cadastro cadastro) {
+        try {
+            String sql = "UPDATE tab_cadastro SET nome = ?, idade = ? WHERE id = ?";
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            pst.setString(1, cadastro.getNome());
+            pst.setInt(2, cadastro.getIdade());
+            pst.setInt(3, cadastro.getId());
+            pst.execute();
+            System.out.println("Cadastro atualizado com sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    ;
+
+    public void excluir(Integer id) {
+        try {
+            String sql = "DELETE FROM public.tab_cadastro WHERE id=?";
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            pst.setInt(1, id);
+            pst.execute();
+            System.out.println("Cadastro excluído com sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    ;
+
+    public List<Cadastro> listarCadastros() {
+        List<Cadastro> lista = new ArrayList<>();
+        Cadastro cadastro = new Cadastro();
+        try {
+            String sql = "SELECT nome, idade, id FROM public.tab_cadastro";
+            PreparedStatement pst = conexao.prepareStatement(sql);
+
+            ResultSet results = pst.executeQuery();
+
+            while (results.next()) {
+                int id = results.getInt("id");
+                String nome = results.getString("nome");
+                int idade = results.getInt("idade");
+                cadastro.setId(id);
+                cadastro.setNome(nome);
+                cadastro.setIdade(idade);
+
+                lista.add(cadastro);
+
+                System.out.println("Cadastro listado com sucesso!");
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return lista;
+    }
+
+
+}
---
+++ src/main/java/SistemaCadastro.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SistemaCadastro {
+
+    public static void main(String[] args) {
+        FabricaConexao.conectar();
+
+        CadastroRepository cadastroRepository = new CadastroRepository();
+        Cadastro cadastro = new Cadastro();
+
+        // Incluir
+       /* cadastro.setNome("Victor Ciechovicz");
+        cadastro.setIdade(30);
+        cadastroRepository.incluir(cadastro);
+        */
+
+        // Atualizar
+       /* cadastro.setId(1);
+        cadastro.setNome("Victor Ciechovicz");
+        cadastro.setIdade(30);
+        cadastroRepository.alterar(cadastro);
+*/
+
+        // Excluir
+        // cadastroRepository.excluir(1);
+
+        //Listar
+        List<Cadastro> lista = cadastroRepository.listarCadastros();
+        for (Cadastro c : lista) {
+            System.out.println(c.getNome());
+        }
+
+    }
+}
